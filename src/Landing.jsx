e358f3d1c3f12346f63a138abb6aa08d6075dcef
craigmacharia <cmacharia482@gmail.com>
2025-06-10 00:18:42 +0300

    prime styling

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -61,16 +61,6 @@ const Landing = () => {
 
   return (
     <div className="landing-page">
-      {/* Curved Decorative Element */}
-      <div className="top-curve">
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-          <path 
-            fill="#0d6efd" 
-            fillOpacity="1" 
-            d="M0,192L48,197.3C96,203,192,213,288,229.3C384,245,480,267,576,250.7C672,235,768,181,864,181.3C960,181,1056,235,1152,234.7C1248,235,1344,181,1392,154.7L1440,128L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z"
-          ></path>
-        </svg>
-      </div>
 
       {/* Expanded Login Modal */}
       {showLoginForm && (
@@ -154,16 +144,7 @@ const Landing = () => {
 
       {/* Hero Section */}
       <section className="hero-section text-white position-relative">
-        {/* New decorative curve at the bottom of the hero section */}
-        <div className="hero-bottom-curve">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 150">
-            <path 
-              fill="#ffffff" 
-              fillOpacity="1" 
-              d="M0,96L48,90.7C96,85,192,75,288,69.3C384,64,480,64,576,80C672,96,768,128,864,128C960,128,1056,96,1152,90.7C1248,85,1344,107,1392,117.3L1440,128L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-            ></path>
-          </svg>
-        </div>
+
         
         <div className="container py-5">
           <div className="row align-items-center">
@@ -202,14 +183,8 @@ const Landing = () => {
 
         {/* Additional decorative curve at the bottom for mobile */}
         <div className="hero-mobile-bottom-curve d-lg-none">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 120">
-            <path 
-              fill="#ffffff" 
-              fillOpacity="1" 
-              d="M0,64L48,80C96,96,192,128,288,128C384,128,480,96,576,96C672,96,768,128,864,138.7C960,149,1056,139,1152,122.7C1248,107,1344,85,1392,74.7L1440,64L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
-            ></path>
-          </svg>
-        </div>
+          </div>
+
       </section>
 
       {/* Trusted By Section */}
